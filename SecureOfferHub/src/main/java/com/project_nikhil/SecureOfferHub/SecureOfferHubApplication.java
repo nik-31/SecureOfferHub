a78caeb0b9cf7dd7d86b16d8eef59e5002b0eeb4
Nikhil Kumar <64148331+nik-31@users.noreply.github.com>
2024-09-14 12:04:14 +0530

    Add files via upload

---
+++ SecureOfferHub/src/main/java/com/project_nikhil/SecureOfferHub/SecureOfferHubApplication.java
@@ -0,0 +1,13 @@
+package com.project_nikhil.SecureOfferHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureOfferHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureOfferHubApplication.class, args);
+	}
+
+}
---
+++ SecureOfferHub/src/test/java/com/project_nikhil/SecureOfferHub/SecureOfferHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project_nikhil.SecureOfferHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureOfferHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
